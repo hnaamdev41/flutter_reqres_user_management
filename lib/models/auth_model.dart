3820ab4f43765bcb5229a56f0528598d3581bb1d
hnaamdev41 <hnaamdev41@gmail.com>
2025-04-05 20:16:27 +0530

    Add authentication and user models

---
+++ lib/models/auth_model.dart
@@ -0,0 +1,43 @@
+class LoginRequest {
+  final String email;
+  final String password;
+
+  LoginRequest({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
+
+class RegisterRequest {
+  final String email;
+  final String password;
+
+  RegisterRequest({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
+
+class AuthResponse {
+  final String? token;
+  final String? error;
+
+  AuthResponse({this.token, this.error});
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      token: json['token'],
+      error: json['error'],
+    );
+  }
+
+  bool get isSuccess => token != null && token!.isNotEmpty;
+}
